ad9af796d62c825fc205a65445cd32715b539872
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2022-09-05 21:31:08 -0400

    created multiple pages

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1 className='text-6xl mb-4 text-white'>Github Pros</h1>
+        <p>Github pros is repo and search engine for top rated github users</p>
+    </div>
+  )
+}
+
+export default About
