984cbb156f43df811c2b5889ab78909ba68c3c43
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2022-09-13 23:08:54 -0400

    created context provider

--- frontend/src/components/users/UserItem.jsx
+++ frontend/src/components/users/UserItem.jsx
@@ -18,7 +18,7 @@ const UserItem = ({user:{login, avatar_url}}) => {
                     {login}
                 </h2>
                 <Link className='text-base-content text-opacity-40' to={`/users/${login}`}>
-                    Visit Profile
+                    View Profile
                 </Link>
             </div>
         </div>
