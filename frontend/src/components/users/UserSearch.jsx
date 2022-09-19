8665efd207134c3da40b9a4a2b26557a89cb373a
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2022-09-18 22:37:34 -0400

    search user context added

--- frontend/src/components/users/UserSearch.jsx
+++ frontend/src/components/users/UserSearch.jsx
@@ -1,25 +1,54 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import GithubContext from '../../context/github/GithubContext'
+
+
 
 const UserSearch = () => {
+
+  const [text, setText] = useState('')
+
+  const { users } = useContext(GithubContext)
+
+  const handleChange = (e) => setText(e.target.value)
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault()
+    if (text === '') {
+      alert('Input field is blank!')
+    } else {
+      setText('')
+    }
+  }
   return (
     <div className='grid grid-cols-1 xl:grid-cols-2 
                     lg:cols-2 md:grid-cols-2 mb-8 gap-8'>
-                      <div>
-                        <form>
-                           <div className='form-control'>
-                            <div className='relative'>
-                              <input className='w-full pr-40 bg-gray-200 input input-lg text-black'
-                              placeholder='Search'/>
-                              <button type='submit' className='absolute top-0 right-0 rounded-l-none w-36 btn btn-lg
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <div className='relative'>
+              <input className='w-full pr-40 bg-gray-200 input input-lg text-black'
+                placeholder='Search'
+                value={text}
+                onChange={handleChange}
+              />
+              <button type='submit' className='absolute top-0 right-0 rounded-l-none w-36 btn btn-lg
                               '>Go</button>
-                            </div>
-                           </div>
-                        </form>
-                      </div>
-                      <div className="button btn btn-ghost btn-lg">
-                        Clear
-                      </div>
-                    </div>
+            </div>
+          </div>
+        </form>
+      </div>
+      {users.length > 0 && (
+
+        <div >
+          <button className="button btn btn-ghost btn-lg">
+            Clear
+          </button>
+
+        </div>
+      )}
+
+    </div>
   )
 }
 
