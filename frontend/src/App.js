6387161f2533b779ccfb6d912268467d8171591f
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2022-10-03 21:19:34 -0400

    alert component created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/Home'
 import NotFound from './pages/NotFound';
 import { GithubProvider } from './context/github/GithubContext';
 import { AlertProvider } from './context/alert/AlertContext';
+import Alert from './components/layouts/Alert'
 
 const  App=()=> {
   return (
@@ -15,6 +16,7 @@ const  App=()=> {
         <div className="flex flex-col justify-between h-screen">
           <Navbar/>
           <main className='container mx-auto px-3 pb-12'>
+            <Alert/>
             <Routes>
               <Route path="/" element={<Home/>} />
               <Route path='/about' element={<About/>}/>
